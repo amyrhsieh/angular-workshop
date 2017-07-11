b25adbe85c97ce7d64e508e78bb3dc09a69f935e
Amy Hsieh <amy.hsieh@washpost.com>
2017-07-11 16:15:09 -0400

    Set up the app routing module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
+import { RouterModule }   from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent }   from './app.component';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule
   ],
   declarations: [
     AppComponent
