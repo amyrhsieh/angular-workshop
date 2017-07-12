60e8906840572b07dd5e5bcfab1a3da009ddd1b6
Amy Hsieh <amy.hsieh@washpost.com>
2017-07-12 15:47:26 -0400

    Added content to Concepts page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { RouterModule }   from '@angular/router';
+import { FormsModule }          from '@angular/forms';
 
 import { AppRoutingModule }   from './app-routing.module';
 import { AppComponent }       from './app.component';
@@ -11,6 +12,7 @@ import { RobotsComponent }    from './robots/robots.component';
   imports: [
     BrowserModule,
     RouterModule,
+    FormsModule,
     AppRoutingModule
   ],
   declarations: [
--- src/app/concepts/concepts.component.ts
+++ src/app/concepts/concepts.component.ts
@@ -3,5 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'concepts',
   templateUrl: './concepts.component.html',
+  styleUrls: [
+    '../styles/buttons.scss',
+    './concepts.component.scss'
+  ]
 })
-export class ConceptsComponent { }
+export class ConceptsComponent {
+  //Data binding
+  simpleName: string = 'Amy';
+
+  //Two-way input binding
+  inputName: string = 'Your name here';
+
+  //ngIf
+  showConditional: boolean = true;
+
+  //ngFor
+  veggies = [
+    'carrot',
+    'broccoli',
+    'cabbage'
+  ]
+
+  //Event binding
+  handleClick(): void {
+    this.showConditional = !this.showConditional;
+  }
+}
