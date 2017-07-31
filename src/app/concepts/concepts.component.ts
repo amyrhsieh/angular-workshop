fb27555f0b9eff9d2e7f824418474ba094259c94
Amy Hsieh <amy.hsieh@washpost.com>
2017-07-31 15:52:36 -0400

    Minor changes to terminology in concepts.

--- src/app/concepts/concepts.component.ts
+++ src/app/concepts/concepts.component.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
   ]
 })
 export class ConceptsComponent {
-  //Data binding
+  //Template interpolation
   simpleName: string = 'Amy';
 
   //Two-way input binding
